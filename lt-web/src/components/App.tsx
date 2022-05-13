ae9a726d8b3ee23f21256630f43b1bbba375c4e7
lautaroturic <lautaroturic@gmail.com>
2022-05-13 18:46:30 -0300

    Revert "."
    This reverts commit 26b4b377cbacebdee3daed813dc47431b758afd2.

--- lt-web/src/components/App.tsx
+++ lt-web/src/components/App.tsx
@@ -2,14 +2,12 @@ import Main from './Main'
 import Header from './Header'
 import Footer from './Footer'
 import { Routes, Route } from 'react-router-dom'
-import About from './About'
 const App = () => {
     return (
         <div id="App" className='min-h-screen flex-col'>
             <Header />
             <Routes>
                 <Route path="/" element={<Main />} />
-                <Route path='about' element={<About />} />
             </Routes>
             <Footer />
         </div>
