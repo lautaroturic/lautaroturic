26b4b377cbacebdee3daed813dc47431b758afd2
lautaroturic <lautaroturic@gmail.com>
2022-05-13 17:22:34 -0300

    .

--- lt-web/src/components/App.tsx
+++ lt-web/src/components/App.tsx
@@ -2,12 +2,14 @@ import Main from './Main'
 import Header from './Header'
 import Footer from './Footer'
 import { Routes, Route } from 'react-router-dom'
+import About from './About'
 const App = () => {
     return (
         <div id="App" className='min-h-screen flex-col'>
             <Header />
             <Routes>
                 <Route path="/" element={<Main />} />
+                <Route path='about' element={<About />} />
             </Routes>
             <Footer />
         </div>
